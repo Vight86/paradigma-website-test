9dc0afba2827a7d078c2953b4a1eaf8fcfee7eb3
Vova Pekun <vpekun@gmail.com>
2020-07-22 15:43:49 +0300

    Adds responsive

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,13 @@
+const menuToggle = document.querySelector('.header__menu-toggle');
+const nav = document.querySelector('.header__nav');
+
+if (menuToggle) {
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('header__menu-toggle_active');
+    nav.classList.toggle('nav_active');
+  });
+}
+
 const popupShow = document.querySelector('.header__btn');
 const success = document.querySelector('.popup__success');
 const form = document.querySelector('.popup__form');
